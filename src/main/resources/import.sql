16b14b2b1e54cb24b4b34483155f6452b3a6d5a5
LuisMunar <luis.emunarr@gmail.com>
2024-05-19 15:44:23 -0500

    Video 123 ready

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO persons (name, lastname, programming_language) VALUES ('Jhon', 'Doe', 'Python');
+INSERT INTO persons (name, lastname, programming_language) VALUES ('Alice', 'Smith', 'Java');
+INSERT INTO persons (name, lastname, programming_language) VALUES ('Bob', 'Johnson', 'JavaScript');
